bfb664f25392eb1c56fab540697d433618cdab19
Dariel Vidal Cotto <vidaldariel93@gmail.com>
2026-06-24 18:12:44 -0400

    Initial commit - VR TicTacToe

---
+++ Assets/Scripts/BoardCell.cs
@@ -0,0 +1,52 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class BoardCell : MonoBehaviour
+{
+   public int row;
+   public int column;
+
+
+   private Renderer cellRenderer;
+   private Color originalColor;
+   public Color hoverColor = Color.cyan;
+
+
+
+   public bool IsOccupied { get; private set; }
+
+
+
+    private void Awake()
+    {
+        cellRenderer = GetComponent<Renderer>();
+        originalColor = cellRenderer.material.color;
+    }
+
+    private void OnMouseEnter()
+    {
+        if (!IsOccupied)
+            cellRenderer.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        cellRenderer.material.color = originalColor;
+    }
+
+
+    public void Select()
+    {
+        // Prevent placing more than one piece in the same cell.
+        if (IsOccupied)
+        {
+            Debug.Log("Cell already occupied");
+            return;
+        }
+
+
+        GameManager.Instance.SelectCell(this);
+        IsOccupied = true;
+    }
+
+}
